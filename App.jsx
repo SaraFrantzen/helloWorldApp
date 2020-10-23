c9d4e868cc1e75714ac2bff505052a4640dd923c
sara <saralundkvist86@gmail.com>
2020-10-23 12:37:33 +0200

    installing react-native-community/async-storage, new module for authentication. Creates new Logincomponent and set that to the first page of the app

--- App.jsx
+++ App.jsx
@@ -4,12 +4,14 @@ import MainScreen from "./src/components/MainScreen";
 import SecondScreen from "./src/components/SecondScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/components/Login";
 
 const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="first page" component={MainScreen} />
         <Stack.Screen name="second page" component={SecondScreen} />
       </Stack.Navigator>
--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-import React from 'react'
+import React from "react";
 import { registerRootComponent } from "expo";
-import { Provider } from 'react-redux';
-import configureStore from './src/state/store/configureStore';
+import { Provider } from "react-redux";
+import configureStore from "./src/state/store/configureStore";
 import App from "./App";
 
-
-
-const store = configureStore()
+const store = configureStore();
 
 const ConnectedApp = () => {
-	return (
-		<Provider store={store}>
-			<App />
-		</Provider>
-	)
-}
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
 
 registerRootComponent(ConnectedApp);
-
-
